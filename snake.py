9c0ac49cae993f5b6a148075a7d43202b3761d5b
Diego <60768726+ZetZed@users.noreply.github.com>
2020-06-10 23:59:37 -0300

    Terceira Parte

--- snake.py
+++ snake.py
@@ -20,12 +20,35 @@ apple.fill((255,0,0))
 
 my_direction = LEFT
 
-while True:
+clock = pygame.time.Clock()
 
+while True:
+    clock.tick(20)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
 
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                my_direction = UP
+            if event.key == K_DOWN:
+                my_direction = DOWN
+            if event.key == K_RIGHT:
+                my_direction = RIGHT
+            if event.key == K_LEFT:
+                my_direction = LEFT
+
+    if my_direction == UP:
+        snake[0] = (snake[0][0], snake [0][1] - 10)
+    if my_direction == DOWN:
+        snake[0] = (snake[0][0], snake [0][1] + 10)
+    if my_direction == RIGHT:
+        snake[0] = (snake[0][0] + 10, snake [0][1])
+    if my_direction == LEFT:
+        snake[0] = (snake[0][0] - 10, snake [0][1])
+
+    for i in range(len(snake)-1,0,-1):
+        snake[i] = (snake[i-1][0], snake[i-1][1])    
 
     screen.fill((0,0,0))
     screen.blit(apple, apple_pos)
